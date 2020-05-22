f141aff013155871bf6f2819cdf1762eb1f4e134
Alexander Teslyuk <aateslyuk@gmail.com>
2020-05-22 20:17:01 +0300

    Added NewsAsync and PatternsAsync.

---
+++ FinnhubCSharpClient/Models/News.cs
@@ -0,0 +1,14 @@
+﻿namespace FinnhubCSharpClient.Models
+{
+    public class News
+    {
+        public int Id;
+        public long Datetime;
+        public string Headline;
+        public string Image;
+        public string Related;
+        public string Source;
+        public string Summary;
+        public string Url;
+    }
+}

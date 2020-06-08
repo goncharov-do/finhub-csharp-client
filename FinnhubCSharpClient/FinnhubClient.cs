4abce2543cd713a9a51b0ab20989a3992d9385de
Alexander Teslyuk <aateslyuk@gmail.com>
2020-06-08 16:53:43 +0300

    Added "token" parameter to methods, added exception throw when status code != 200.

--- FinnhubCSharpClient/FinnhubClient.cs
+++ FinnhubCSharpClient/FinnhubClient.cs
@@ -1,8 +1,8 @@
 ﻿using FinnhubCSharpClient.Models;
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace FinnhubCSharpClient
@@ -10,40 +10,39 @@ namespace FinnhubCSharpClient
     public class FinnhubClient
     {
         private readonly HttpClient _client = new HttpClient();
-        private readonly string _token;
 
-        public FinnhubClient(Uri baseAddress, string token)
+        public FinnhubClient(Uri baseAddress)
         {
             _client.BaseAddress = baseAddress;
-            _token = token;
         }
 
-        private async Task<HttpResponseMessage> Get(string request)
+        private async Task<HttpResponseMessage> Get(string request, string token)
         {
-            return await _client.GetAsync($"{request}&token={_token}");
+            return await _client.GetAsync($"{request}&token={token}");
         }
 
-        private bool CanRead(HttpResponseMessage response)
+        public async Task<IEnumerable<StockSymbol>> StockSymbolsAsync(string exchange, string token)
         {
-            return response.Content.Headers.ContentType.MediaType == "application/json";
+            var response = await Get($"stock/symbol?exchange={exchange}", token);
+            if (response.StatusCode != HttpStatusCode.OK)
+                throw new Exception();
+            return await response.Content.ReadAsAsync<IEnumerable<StockSymbol>>();
         }
 
-        public async Task<IEnumerable<StockSymbol>> StockSymbolsAsync(string exchange)
+        public async Task<IEnumerable<News>> NewsAsync(string stock, DateTime from, DateTime to, string token)
         {
-            var response = await Get($"stock/symbol?exchange={exchange}");
-            return CanRead(response) ? await response.Content.ReadAsAsync<IEnumerable<StockSymbol>>() : null;
+            var response = await Get($"company-news?symbol={stock}&from={from:yyyy-MM-dd}&to={to:yyyy-MM-dd}", token);
+            if (response.StatusCode != HttpStatusCode.OK)
+                throw new Exception();
+            return await response.Content.ReadAsAsync<IEnumerable<News>>();
         }
 
-        public async Task<IEnumerable<News>> NewsAsync(string stock, DateTime from, DateTime to)
+        public async Task<PatternResponse> PatternsAsync(string stock, string resolution, string token)
         {
-            var response = await Get($"company-news?symbol={stock}&from={from:yyyy-MM-dd}&to={to:yyyy-MM-dd}");
-            return CanRead(response) ? await response.Content.ReadAsAsync<IEnumerable<News>>() : null;
-        }
-
-        public async Task<PatternResponse> PatternsAsync(string stock, string resolution)
-        {
-            var response = await Get($"scan/pattern?symbol={stock}&resolution={resolution}");
-            return CanRead(response) ? await response.Content.ReadAsAsync<PatternResponse>() : null;
+            var response = await Get($"scan/pattern?symbol={stock}&resolution={resolution}", token);
+            if (response.StatusCode != HttpStatusCode.OK)
+                throw new Exception();
+            return await response.Content.ReadAsAsync<PatternResponse>();
         }
     }
 }

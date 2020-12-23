90602b18cbdf04a70a0056595085f5a73eb2e3d6
Pavel Ryabinin <rpk9810@gmail.com>
2020-12-23 15:57:03 +0500

    Added number of remaining requests and limit reset time

---
+++ FinnhubCSharpClient/FinnhubResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinnhubCSharpClient
+{
+    public class FinnhubResponse<T>
+    {
+        public int RemainingRequests { get; set; }
+        public DateTimeOffset LimitResetTime { get; set; }
+        public T Data { get; set; }
+    }
+}
